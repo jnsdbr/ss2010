b19dba3bbe70b3a8415dc3294a74c9d3ab0b2a0c
Jens <nicer@zuglufttier.de>
2010-05-06 16:27:27 +0200

    More

--- p05/TextSpeicher.cpp
+++ p05/TextSpeicher.cpp
@@ -2,11 +2,55 @@
 
 using namespace std;
 
+void TextSpeicher::expand(int s)
+{
+	
+	if(s > lines)
+	{
+		TextZeile **tmpArray = new *TextZeile [size + s];
+		
+		for(int i = 0; i < s; i++)
+		{
+			tmpArray[i] = (t[i] != NULL) ? t[i] : NULL;
+		}
+		
+		delete [] t;
+		
+		t = tmpArray;		
+		
+		lines = s;
+	}
+}
 TextSpeicher::TextSpeicher()
-{	
+{
+	size = 0;
+	lines = 0;
+	t = NULL;
+	filename = NULL;
 }
 TextSpeicher::TextSpeicher(string Filename)
 {
+	size = 0;
+	lines = 0;
+	t = NULL;
+	
+	ifstream in(Filename.c_str());
+	
+	try
+	{
+		if(in)
+		{
+			while(!in.eof())
+			{
+				//getline(in, ,'\n');
+			}
+			in.close();
+		}	
+	}
+	catch(...)
+	{
+		
+	}
 }
 TextSpeicher::TextSpeicher(TextSpeicher&)
 {
@@ -19,7 +63,10 @@ TextSpeicher& TextSpeicher::TextSpeicher::operator= (TextSpeicher&)
 }
 void TextSpeicher::SetFilename(string Filename)
 {
-	filename = Filename;
+	if(!Filename.empty())
+	{
+		filename = Filename;
+	}
 }
 TextSpeicher::TextZeile& TextSpeicher::operator [] (int line)
 {	
